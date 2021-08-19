877de02346a012cb477bc1aedf0fd0d2d870ca02
Andreas Metsälä <andreas.metsala@dalinjun.com>
2021-08-19 14:52:35 +0300

    feat: add basic bootstrap target which installs Homebrew
    Signed-off-by: Andreas Metsälä <andreas.metsala@dalinjun.com>

---
+++ devenv/devenv.mk
@@ -0,0 +1,13 @@
+BOOTSTRAP_INIT_FOUND := $(shell ! grep -q -e "^bootstrap.init:" $(word 1,$(MAKEFILE_LIST)); echo $$?)
+BOOTSTRAP_POST_FOUND := $(shell ! grep -q -e "^bootstrap.post:" $(word 1,$(MAKEFILE_LIST)); echo $$?)
+
+bootstrap: #info: Bootstrap development environment
+ifeq ($(BOOTSTRAP_INIT_FOUND),1)
+	@make bootstrap.init
+endif
+
+	@$(PROJECT_ROOT)/devenv/bootstrap
+
+ifeq ($(BOOTSTRAP_POST_FOUND),1)
+	@make bootstrap.post
+endif
